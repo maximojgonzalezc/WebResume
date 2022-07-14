47e9f8ce1a5435646f2508fb25695cb4083db6e1
maximojgonzalezc <maximo.j.gonzalez.c@gmail.com>
2022-07-14 18:45:59 -0500

    updated social media

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
+    <a href="https://www.facebook.com/maaxii.91/">
+      <div>
+        <FaFacebookF />
+      </div>
+    </a>
+    <a href="https://www.instagram.com/mcseemo._/">
+      <div>
+        <BsInstagram />
+      </div>
+    </a>
   </div>
 );
 
